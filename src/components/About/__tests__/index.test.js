4538bf3f621e6cf911cbad20be3b41e303071916
Johnathan Shields <Johnathan.Shields@buhlergroup.com>
2022-04-05 18:42:09 -0500

    code snapshot

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -1,24 +1,19 @@
+// __tests__/About.test.js
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import About from '../index'
+import About from '..';
 
 afterEach(cleanup);
 
-describe('about component', () => {
-    // renders About test
-
-    // first test - verify component is rendering
-    it('renders',  ()=> {
-        render(<About />);
-    });
-    // 2nd test
-    it('matches snapshot DOM node structure', ()=> {
-        // render About
-        const { asFragment } = render(<About />)
-        expect(asFragment()).toMatchSnapshot();
-    })
-
-
-
+describe('About component', () => {
+  it('renders', () => {
+    render(<About />);
+  });
+  
+  it('matches snapshot DOM node structure', () => {
+    const { asFragment } = render(<About />);
+    
+    expect(asFragment()).toMatchSnapshot();
+  });
 })
